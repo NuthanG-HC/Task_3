28565ca754b8e0cdc6c2e9bbe881c58942dfb981
NuthanG-HC <116954346+NuthanG-HC@users.noreply.github.com>
2023-02-12 17:18:08 +0530

    changes made to API's

--- script.js
+++ script.js
@@ -3,11 +3,10 @@
 document.getElementById('action').addEventListener('click',(e)=>{
     let param = document.getElementById("title").value
     if(param!=""){
-    fetch("https://www.omdbapi.com/?i=tt3896198&apikey=844a9c20")
+    fetch(`https://www.omdbapi.com/?apikey=844a9c20&t=${param}`)
     .then(res=>res.json())
     .then(data =>{
-       if(data.Title.toLowerCase() == param.toLowerCase()){
-         document.getElementById("img").setAttribute('src',"https://img.omdbapi.com/?i=tt3896198&apikey=844a9c20")
+         document.getElementById("img").setAttribute('src',`https://img.omdbapi.com/?apikey=844a9c20&&i=${data.imdbID}`)
           document.getElementById("lists").innerHTML= (`<ul>
           <li style="font-size:28px;
           font-weight:bold">Movie title:${data.Title}</li>
@@ -15,8 +14,7 @@ document.getElementById('action').addEventListener('click',(e)=>{
          <li>IMDB rating:${data.imdbRating}</li>
          <li>Cast and Crew:${data.Director},${data.Writer},${data.Actors}</li>
          </ul>`)
-       }})
+       })
     .catch(err =>console.log(err));
-    }
-  
+      }
 })
